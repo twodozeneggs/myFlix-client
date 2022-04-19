20f588156ba52472141b40efcbe1790bd9de250b
twodozeneggs <simonjohnloik@gmail.com>
2022-04-19 16:12:13 -0500

    Create main-view.jsx

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+    render (){
+        return (
+            <div classname="main-view">
+                <div>Inception</div>
+                <div>The Shawshank Redemption</div>
+                <div>Gladiator</div>
+            </div>
+        );
+    }
+}
