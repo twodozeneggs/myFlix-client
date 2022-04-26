53f9c1f03bc66402678fd6b6e13d42d558044987
twodozeneggs <simonjohnloik@gmail.com>
2022-04-26 18:54:36 -0500

    i'm going through changes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,36 +5,34 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
+  constructor() {
+    // same code
+  }
 
-    constructor(){
-        super();
-        this.state = {
-          movies: [],
-          selectedMovie: null
-        }
-      }
-
-      componentDidMount(){
-        axios.get('https://myflix.herokuapp.com/movies')
-        .then(response => {
-          this.setState({
-            movies: response.data
-          });
-        })
-      }
+  componentDidMount(){
+    axios.get('https://[APP-NAME].herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
       .catch(error => {
         console.log(error);
       });
-    }
+  }
 
-      setSelectedMovie(newSelectedMovie){
-          this.setState({
-               selectedMovie: newSelectedMovie
-            });
-      }
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      SelectedMovie: newSelectedMovie
+   });
+  }
 
+ render() {
+    const { movies, selectedMovie } = this.state;
 
-      return (
+    if (movies.length === 0) return <div className="main-view" />;
+
+    return (
       <div className="main-view">
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
